813dd036312a6ce2ff7e4994fe2ae4e61471ef1c
Jason Morris <jmail.morris@gmail.com>
2020-12-22 00:56:24 +0000

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ function IndexPage() {
     <Layout>
       <SEO title="Home" />
       <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
+      <h1>Help save the Environment while earning rewards</h1>
+      <p>For people who want to drive change and make a difference with helping save the environment.</p>
       <Link to="/page-2/">Go to page 2</Link> <br />
     </Layout>
   )
