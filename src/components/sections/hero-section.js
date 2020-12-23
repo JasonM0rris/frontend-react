792dd6ec6d67f56b97d3f9aa9075581d674af5c3
Jason Morris <jmail.morris@gmail.com>
2020-12-23 02:05:28 +0000

    Added styled library. Added few elements.

--- src/components/sections/hero-section.js
+++ src/components/sections/hero-section.js
@@ -1,18 +1,49 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 function HeroSection() {
   return (
-    <div>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Help save the Environment while earning rewards</h1>
-      <p>
-        For people who want to drive change and make a difference with helping
-        save the environment.
-      </p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </div>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>
+            Do your <br />
+            bit for a greener globe, while earning rewards
+          </Title>
+          <Description>
+            For people who want to drive change and make a difference with
+            helping save the environment.
+          </Description>
+          <Link to="/page-2/">Go to page 2</Link> <br />
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 
 export default HeroSection
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  padding: 200px 30px;
+  margin: 0 auto;
+`
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`
+
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`
